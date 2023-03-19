e7052d7a53377950f17571f42dd2a1482810f175
jahid <mdjahidulislam2852@gmail.com>
2023-03-20 00:22:30 +0600

    add backend code

---
+++ combind-app/my-app/app.js
@@ -0,0 +1,41 @@
+const  express=require('express');
+const router=require('./src/route/api')
+const app=new express();
+const bodyParser=require('body-parser');
+
+
+//security middleware
+
+const rateLimit=require('express-rate-limit');
+const  helmet=require('helmet');
+const mongoSanitize=require('express-mongo-sanitize');
+const xss=require('xss-clean');
+const hpp=require('hpp');
+const cors=require('cors');
+
+
+
+//Database
+const mongoose=require('mongoose');
+
+
+//Security Middleware
+app.use(cors());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp())
+
+
+//Body perser
+app.use(bodyParser.json())
+
+//Rate Limiter
+const limiter=rateLimit({windowMs:15*60*100,max:300})
+
+//Database
+app.use("/api/v1",router)
+
+module.exports=app;
+
+
